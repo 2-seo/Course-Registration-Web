16c092a3a8ead6119cd26713a257c0db5690ac42
Harry-Bro <likeharrybro@gmail.com>
2021-02-12 20:40:46 +0900

    [add] 장바구니 Controller, Service 레이어 분할

---
+++ src/main/java/com/harrybro/courseregistration/domain/university/service/BasketService.java
@@ -0,0 +1,33 @@
+package com.harrybro.courseregistration.domain.university.service;
+
+import com.harrybro.courseregistration.domain.account.domain.Account;
+import com.harrybro.courseregistration.domain.account.repository.AccountReposiitory;
+import com.harrybro.courseregistration.domain.university.domain.Lecture;
+import com.harrybro.courseregistration.domain.university.repository.LectureRepository;
+import com.harrybro.courseregistration.global.config.auth.PrincipalDetail;
+import com.harrybro.courseregistration.global.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BasketService {
+
+    private final AccountReposiitory accountReposiitory;
+    private final LectureRepository lectureRepository;
+
+    public ResponseDto deleteLectureInBasket(Long lectureID, PrincipalDetail principal) {
+        Account account = accountReposiitory.findByUsername(principal.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다."));
+        Lecture lecture = lectureRepository.findById(lectureID)
+                .orElseThrow(() -> new IllegalArgumentException("해당 강의가 존재하지 않습니다."));
+        account.getBasket().deleteLecture(lecture);
+
+        return ResponseDto.builder()
+                .status(HttpStatus.OK)
+                .message("선택한 과목이 장바구니에서 삭제되었습니다.")
+                .build();
+    }
+}
