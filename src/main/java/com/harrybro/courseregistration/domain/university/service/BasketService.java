ab91a43b08d522c4baf70338c72a384ecb5577ba
Harry-Bro <likeharrybro@gmail.com>
2021-05-14 09:14:08 +0900

    refactor: Refactor BasketService

--- src/main/java/com/harrybro/courseregistration/domain/university/service/BasketService.java
+++ src/main/java/com/harrybro/courseregistration/domain/university/service/BasketService.java
@@ -1,14 +1,13 @@
 package com.harrybro.courseregistration.domain.university.service;
 
-import com.harrybro.courseregistration.domain.user.domain.User;
-import com.harrybro.courseregistration.domain.user.repository.UserRepository;
 import com.harrybro.courseregistration.domain.university.domain.lecture.Lecture;
-import com.harrybro.courseregistration.domain.university.repository.LectureRepository;
+import com.harrybro.courseregistration.domain.university.dto.UserAndLectureDto;
+import com.harrybro.courseregistration.domain.university.util.UserAndLectureValidator;
+import com.harrybro.courseregistration.domain.user.domain.User;
 import com.harrybro.courseregistration.global.config.auth.PrincipalDetail;
 import com.harrybro.courseregistration.global.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,19 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class BasketService {
 
-    private final UserRepository userRepository;
-    private final LectureRepository lectureRepository;
+    private final UserAndLectureValidator userAndLectureValidator;
 
     @Transactional
     public ResponseDto<?> saveLecture(Long lectureID, PrincipalDetail principal) {
-        User user = userRepository.findByUsername(principal.getUsername())
-                .orElseThrow(() -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다."));
-        Lecture lecture = lectureRepository.findById(lectureID)
-                .orElseThrow(() -> new IllegalArgumentException("해당 강의가 존재하지 않습니다."));
-
-        if (user.getBasket().getLectures().stream().anyMatch(l -> l == lecture))
+        UserAndLectureDto validate = userAndLectureValidator.validate(principal, lectureID);
+        User user = validate.getUser();
+        Lecture lecture = validate.getLecture();
+        if (user.getBasket().getLectures().stream().anyMatch(l -> l.equals(lecture)))
             throw new IllegalArgumentException("이미 장바구니에 담긴 강의입니다.");
-
         user.getBasket().saveLecture(lecture);
 
         return ResponseDto.builder()
@@ -39,11 +34,9 @@ public class BasketService {
 
     @Transactional
     public ResponseDto<?> deleteLecture(Long lectureID, PrincipalDetail principal) {
-        User user = userRepository.findByUsername(principal.getUsername())
-                .orElseThrow(() -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다."));
-        Lecture lecture = lectureRepository.findById(lectureID)
-                .orElseThrow(() -> new IllegalArgumentException("해당 강의가 존재하지 않습니다."));
-
+        UserAndLectureDto validate = userAndLectureValidator.validate(principal, lectureID);
+        User user = validate.getUser();
+        Lecture lecture = validate.getLecture();
         user.getBasket().deleteLecture(lecture);
 
         return ResponseDto.builder()
